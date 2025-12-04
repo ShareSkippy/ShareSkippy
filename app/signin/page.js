004882c1312c7fee27471b04a8dbe156a562987f
Yasar Sadozai <yasarsadozai@gmail.com>
2025-12-04 13:40:25 -0600

    update redirect url

--- app/signin/page.js
+++ app/signin/page.js
@@ -62,7 +62,7 @@ export default function Login() {
 
     try {
       const { type, provider } = options;
-      const redirectURL = 'http://localhost:3000/app/auth/callback/page';
+      const redirectURL = `${process.env.NEXT_PUBLIC_BASE_URL}/app/auth/callback/page`;
       if (type === 'oauth') {
         // Use Supabase's built-in OAuth but with custom branding
         const { error } = await supabase.auth.signInWithOAuth({
